91710cd956cca65064d66f8eca1b28d56ce781ee
nehaTom <neha.tomar@codemaven.in>
2018-04-06 17:57:08 +0530

    firstCommit

---
+++ app/src/main/java/com/example/codemaven3015/sarainternational/Payment_page.java
@@ -0,0 +1,13 @@
+package com.example.codemaven3015.sarainternational;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Payment_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_page);
+    }
+}
