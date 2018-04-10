d48b5f964293eb8522ec104134cbcc711858c19d
nehaTom <neha.tomar@codemaven.in>
2018-04-10 16:04:47 +0530

    firstCommit

---
+++ app/src/main/java/com/example/codemaven3015/sarainternational/Payment_Address.java
@@ -0,0 +1,13 @@
+package com.example.codemaven3015.sarainternational;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Payment_Address extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment__address);
+    }
+}
