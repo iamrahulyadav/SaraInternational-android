b5a2c96b57166974b4be73d138d80aa8e44bb0d5
nehasinghtomar@gmail.com <nehasinghtomar@gmail.com>
2018-04-10 14:52:54 +0530

    Navigation drawer

---
+++ app/src/main/java/com/example/codemaven3015/sarainternational/EditAddress.java
@@ -0,0 +1,13 @@
+package com.example.codemaven3015.sarainternational;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_address);
+    }
+}
