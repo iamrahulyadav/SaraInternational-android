4eff1c7d2191e6dd7aefdf4375b3f8e4a9e40fcc
nehaTom <neha.tomar@codemaven.in>
2018-04-12 15:32:15 +0530

    firstCommit

---
+++ app/src/main/java/com/example/codemaven3015/sarainternational/My_Order_Details.java
@@ -0,0 +1,13 @@
+package com.example.codemaven3015.sarainternational;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class My_Order_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my__order__details);
+    }
+}
