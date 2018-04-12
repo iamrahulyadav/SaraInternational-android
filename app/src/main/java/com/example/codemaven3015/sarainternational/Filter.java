c026dc2ff8b4d2210d9f243ab032ed2b14271508
nehasinghtomar@gmail.com <nehasinghtomar@gmail.com>
2018-04-12 15:15:04 +0530

    Navigation drawer

---
+++ app/src/main/java/com/example/codemaven3015/sarainternational/Filter.java
@@ -0,0 +1,124 @@
+package com.example.codemaven3015.sarainternational;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Filter extends AppCompatActivity {
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+        setClickListnerForExpandable();
+    }
+    public void setClickListnerForExpandable(){
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+    }
+
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Category");
+        listDataHeader.add("Type");
+        listDataHeader.add("Color");
+
+        // Adding child data
+        List<String> category = new ArrayList<String>();
+        category.add("category 1");
+        category.add("category 2");
+        category.add("category 3");
+        category.add("category 4");
+
+        List<String> Type = new ArrayList<String>();
+        Type.add("Type 1");
+        Type.add("Type 2");
+        Type.add("Type 3");
+        Type.add("Type 4");
+        Type.add("Type 5");
+
+        List<String> Color = new ArrayList<String>();
+        Color.add("#fcff00");
+        Color.add("#f33333");
+        Color.add("#fcff00");
+        Color.add("#f33333");
+        Color.add("#fcff00");
+
+        listDataChild.put(listDataHeader.get(0), category); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), Type);
+        listDataChild.put(listDataHeader.get(2), Color);
+    }
+}
